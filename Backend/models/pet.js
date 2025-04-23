c27702e2c4eb91151ca60d06c7848a09d531e75f
astFat <FATIMAZZAHRAE.ASTITOU.ESTN24@ump.ac.ma>
2025-04-23 23:02:08 +0100

    Update pet.js

--- Backend/models/pet.js
+++ Backend/models/pet.js
@@ -1,10 +1,34 @@
 const mongoose = require('mongoose');
 
 const petSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    species: { type: String, required: true },
-    breed: { type: String, required: true },
-    ownerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Owner', required: true },
-}, { timestamps: true });
+  owner: {  
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Owner',
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  birthDate: {
+    type: Date,
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ['bird', 'cat', 'dog', 'hamster', 'lizard'],
+    required: true,
+  },
+});
+
+petSchema.virtual('visits', {
+  ref: 'Visit',
+  localField: '_id',
+  foreignField: 'pet',
+  justOne: false 
+});
+
+petSchema.set('toJSON', { virtuals: true });
+petSchema.set('toObject', { virtuals: true });
 
 module.exports = mongoose.model('Pet', petSchema);
