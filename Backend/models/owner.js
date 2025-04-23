715382e2478d02d2d9f55335496b76590c724ff1
astFat <FATIMAZZAHRAE.ASTITOU.ESTN24@ump.ac.ma>
2025-04-23 23:01:36 +0100

    Update owner.js

--- Backend/models/owner.js
+++ Backend/models/owner.js
@@ -1,10 +1,36 @@
 const mongoose = require('mongoose');
 
 const ownerSchema = new mongoose.Schema({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    address: { type: String, required: true },
-    phone: { type: String, required: true },
-}, { timestamps: true });
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  telephone: {
+    type: String,
+    required: true
+  },
+  pets: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Pet',
+    required: false 
+  }],
+  visits: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Visit',
+    required: false
+  }]
+});
 
 module.exports = mongoose.model('Owner', ownerSchema);
