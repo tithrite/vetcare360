968a7f8ea8985aea4be9671bd7b34ff55e2f2792
astFat <FATIMAZZAHRAE.ASTITOU.ESTN24@ump.ac.ma>
2025-04-23 23:00:41 +0100

    Update Veterinarian.js

--- Backend/models/Veterinarian.js
+++ Backend/models/Veterinarian.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
 
 const veterinarianSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    specialty: { type: String, required: true },
-}, { timestamps: true });
+  name: {
+    type: String,
+    required: true
+  },
+  specialties: {
+    type: [String], 
+    required: true
+  }
+});
 
 module.exports = mongoose.model('Veterinarian', veterinarianSchema);
