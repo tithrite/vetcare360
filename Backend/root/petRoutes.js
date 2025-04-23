a7db5f34b66415fbc4789e5be8948ef0c50ba6e4
tithrite <TITHRITE.AITADDI.ESTN24@ump.ac.ma>
2025-04-23 23:58:31 +0200

    Update pet.routes.js

--- Backend/root/petRoutes.js
+++ Backend/root/petRoutes.js
@@ -1,38 +1,4 @@
 const express = require('express');
-const Pet = require('../models/Pet');
 const router = express.Router();
-
-// Ajouter un animal
-router.post('/', async (req, res) => {
-    const pet = new Pet(req.body);
-    try {
-        const newPet = await pet.save();
-        res.status(201).json(newPet);
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
-});
-
-// Modifier un animal
-router.put('/:id', async (req, res) => {
-    try {
-        const pet = await Pet.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        if (!pet) return res.status(404).json({ message: 'Animal non trouvé' });
-        res.json(pet);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-});
-
-// Supprimer un animal
-router.delete('/:id', async (req, res) => {
-    try {
-        const pet = await Pet.findByIdAndDelete(req.params.id);
-        if (!pet) return res.status(404).json({ message: 'Animal non trouvé' });
-        res.json({ message: 'Animal supprimé avec succès' });
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-});
 
 module.exports = router;
