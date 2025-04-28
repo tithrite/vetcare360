059cfc6aba97139e73017206799826bb993a5ead
tithrite <TITHRITE.AITADDI.ESTN24@ump.ac.ma>
2025-04-28 22:25:37 +0200

    Update ownerRoutes.js

--- Backend/root/ownerRoutes.js
+++ Backend/root/ownerRoutes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const ownerController = require('../controllers/owner.controller');
 
+router.post('/', ownerController.createOwner);
+router.get('/', ownerController.getOwners);
+router.get('/:id', ownerController.getOwnerById);
 router.get('/search/:lastName', ownerController.getOwnerByLastName); 
+router.put('/:id', ownerController.updateOwner);
 
 module.exports = router;
