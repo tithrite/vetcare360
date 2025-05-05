a6191239d09642d356ac719b975897c5662f1ebb
tithrite <TITHRITE.AITADDI.ESTN24@ump.ac.ma>
2025-05-06 00:45:44 +0200

    Update visitRoutes.js

--- Backend/root/visitRoutes.js
+++ Backend/root/visitRoutes.js
@@ -1,4 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const visitController = require('../controllers/visit.controller');
+
+router.post('/', visitController.createVisit);
 
 module.exports = router;
