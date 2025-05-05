d3e037584683139eddfee0f073185eb7225bcd64
tithrite <TITHRITE.AITADDI.ESTN24@ump.ac.ma>
2025-05-06 00:43:38 +0200

    Update petRoutes.js

--- Backend/root/petRoutes.js
+++ Backend/root/petRoutes.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const petController = require('../controllers/pet.controller');
+
+router.post('/', petController.createPet);
+router.get('/types', petController.getPetTypes); 
+router.get('/:id', petController.getPetById);
+router.put('/:id', petController.updatePet);
 
 module.exports = router;
