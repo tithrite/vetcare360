a9ff978dcab50eb159a8af01e5e9d2110f934224
tithrite <TITHRITE.AITADDI.ESTN24@ump.ac.ma>
2025-05-06 00:39:00 +0200

    Update visit.controller.js

--- Backend/controllers/visit.controller.js
+++ Backend/controllers/visit.controller.js
@@ -1,1 +1,11 @@
+const Visit = require('../models/visit.model');
 
+exports.createVisit = async (req, res) => {
+  try {
+    const newVisit = new Visit(req.body);
+    await newVisit.save();
+    res.status(201).json(newVisit);
+  } catch (err) {
+    res.status(500).json({ message: 'Error creating visit', error: err });
+  }
+};
